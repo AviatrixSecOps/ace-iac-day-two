fafda5db7019efe0a052c3cb5be3a347538bdabd
AviatrixSecOps <98222861+AviatrixSecOps@users.noreply.github.com>
2022-01-27 12:33:54 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,5 +1,8 @@
 terraform {
   required_version = ">= 1.0.2"
+  credentials "app.terraform.io" {
+  token = "uAyaMYHykjFc2Q.atlasv1.NA1h6WR2nFm9cpNnzKeJEVwLUkC6IOMqz0Neo0ZswJIku4F9BrlOIbZM59242zBx6Rw"
+}
   backend "remote" {
     organization = "mderaet"
     workspaces {
